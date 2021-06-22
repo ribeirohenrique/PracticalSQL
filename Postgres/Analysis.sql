83806b6509ffc23ae0db5523f4a615f42ca67700
Henrique <henrique1258@gmail.com>
2021-06-22 17:18:48 -0300

    Mais exercícios

--- Postgres/Analysis.sql
+++ Postgres/Analysis.sql
@@ -137,3 +137,66 @@ select numeric_column,
 from number_data_types;
 
 select cast(char_column as integer) from char_data_types;
+
+---listing 4-1:
+---Esse comando copy não funciona se for de uma máquina diferente do host, usei a importação através do Datagrip mesmo
+COPY table_name
+FROM 'C:\YourDirectory\your_file.csv'
+WITH (FORMAT CSV, HEADER);
+
+---listing 4-3:
+---Esse comando copy não funciona se for de uma máquina diferente do host, usei a importação através do Datagrip mesmo
+COPY us_counties_2010
+FROM 'C:\YourDirectory\us_counties_2010.csv'
+WITH (FORMAT CSV, HEADER);
+
+SELECT * from us_counties_2010;
+
+SELECT geo_name, state_us_abbreviation, area_land
+FROM us_counties_2010
+ORDER BY area_land DESC
+LIMIT 3;
+
+SELECT geo_name, state_us_abbreviation, internal_point_lon
+FROM us_counties_2010
+ORDER BY internal_point_lon DESC
+LIMIT 5;
+
+---listing 4-4:
+CREATE TABLE supervisor_salaries (
+town varchar(30),
+county varchar(30),
+supervisor varchar(30),
+start_date date,
+salary money,
+benefits money
+);
+
+---listing 4-5:
+---Esse comando copy não funciona se for de uma máquina diferente do host, usei a importação através do Datagrip mesmo
+COPY supervisor_salaries (town, supervisor, salary)
+FROM 'C:\YourDirectory\supervisor_salaries.csv'
+WITH (FORMAT CSV, HEADER);
+
+select * from supervisor_salaries;
+
+DELETE FROM supervisor_salaries;
+
+---listing 4-6:
+---Esse comando copy não funciona se for de uma máquina diferente do host, usei a importação através do Datagrip mesmo
+CREATE TEMPORARY TABLE supervisor_salaries_temp (LIKE supervisor_salaries);
+
+COPY supervisor_salaries_temp (town, supervisor, salary)
+FROM 'D:\Desenvolvimento\practical-sql\Chapter_04\supervisor_salaries.csv'
+WITH (FORMAT CSV, HEADER);
+
+INSERT INTO supervisor_salaries (town, county, supervisor, salary)
+SELECT town, 'Some County', supervisor, salary FROM supervisor_salaries_temp;
+
+drop table supervisor_salaries_temp;
+
+---listing 4-7:
+---Esse comando copy não funciona se for de uma máquina diferente do host, usei a importação através do Datagrip mesmo
+COPY us_counties_2010
+TO 'D:\Desenvolvimento\supervisor_salaries.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER '|');
